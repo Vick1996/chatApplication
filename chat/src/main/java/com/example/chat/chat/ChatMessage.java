7bf5447d9b1bbac7d09539acc122bcec9290534c
Girish <girishk007.gk@gmail.com>
2024-09-02 16:33:56 +0530

    Initial commit

---
+++ chat/src/main/java/com/example/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatMessage
+{
+    private String content;
+    private String sender;
+    private MessageType type;
+}
